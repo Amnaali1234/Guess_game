b8a12a4f3925c353d25de0df468d8fe730e8ba56
Amnaali1234 <amnasheikh2255@gmail.com>
2026-05-02 01:00:36 +0500

    Guess game

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ if (numInput < 1 || numInput > 10) {
      message.innerHTML = "⚠ Please enter a number between 1 to 10";
 }
 else if (numInput === secretNum) {
-    message.innerHTML = "Bingo! Correct Answer🎉❤"
-} else {
-    message.innerHTML = "Wrong answer❌"
+    message.innerHTML = "Bingo! Correct Answer✅🎉💗"
+ } else if (numInput < secretNum) {
+
+        message.innerHTML = "❌ Wrong Answer <br> 📉 Too Low!";
+
+    } else {
+
+        message.innerHTML = "❌ Wrong Answer <br> 📈 Too High!";
+
+   
 }
-if (numInput < secretNum) {
-     message.innerHTML = "📉 Too Low!"
-} else {
-     message.innerHTML = "📈 Too High!"
-}
+
 }
